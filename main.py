04ed2de091ba66a99512a484b60e8fd129e09e34
pzh97 <55832928+pzh97@users.noreply.github.com>
2021-02-22 13:40:25 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-#perform tokenisation and define a training function. 
+import gensim
+import numpy as np
+import pandas as pd
+
+#define a training function
 def train_w2v(path,outpath):
     data=[i.strip() for i in open(path)]
     data=[i.split(" ") for i in data]
-    import gensim
     w2v_model1 = gensim.models.Word2Vec(data, size=128, iter=10, min_count=0)
     word_vectors1 = w2v_model1.wv
     w2v_model1.save(outpath)
@@ -75,8 +78,8 @@ model_1 = Word2Vec.load("w2v1")
 model_2 = Word2Vec.load("w2v2")
 smart_procrustes_align_gensim(model_1, model_2, words=None)
 
-#compute the cosine similarity of target words. 
-import numpy as np
+
+#compute the cosine similarity of target words
 t1  = np.array([-0.4,0.8,0.5,-0.2,0.3])
 t2  = np.array([-0.5,0.4,-0.2,0.7,-0.1])
 def cos_sim(a, b):
@@ -88,10 +91,9 @@ cos_sim(t1,t2)
 targets=[i.strip() for i in open("targets.txt")]
 sim=[]
 for i in targets:
-    sim.append(cos_sim(w2v1[i],w2v2[i]))
+    sim.append(cos_sim(model_1[i], model_2[i]))
 
 #create a csv file that stores the cosine similarity of the target words 
-import pandas as pd
 df=pd.DataFrame()
 df["targets"]=targets
 df["sim"]=sim
