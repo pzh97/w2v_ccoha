062653880e0d488af06b8b345866a05cdbbd8786
pzh97 <55832928+pzh97@users.noreply.github.com>
2021-03-03 00:28:34 +0000

    Update token_frequency.py

--- token_frequency.py
+++ token_frequency.py
@@ -1,11 +1,13 @@
 import nltk
 
+#define a function that tokenises corpora
 def tokenise(corpus):
 	with open(corpus) as myfile:
 		data = myfile.read().replace('\n', ' ')
 	corpus = data.split(' ')
 	return corpus
 
+#define a function that counts the token number of corpora
 def count(data):
     data = tokenise(data)
     fdist1 = nltk.FreqDist(data)
@@ -13,6 +15,7 @@ def count(data):
 count('ccoha1.txt')
 count('ccoha2.txt')
 
+#define a function that counts the token frequency of a list of target words
 def countvoc(corpus, targets):
     corpus = tokenise(corpus)
     fdist1 = nltk.FreqDist(corpus)
