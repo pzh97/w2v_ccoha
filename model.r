2f653f5356fa90076230ea208b12f0118dc69094
pzh97 <55832928+pzh97@users.noreply.github.com>
2021-03-15 14:20:53 +0000

    Update model.r

--- model.r
+++ model.r
@@ -19,16 +19,14 @@ my_data$frequency <- log10(my_data$frequency)
 my_data$polysemy <- log10(my_data$polysemy)
 
 #check normality after transformation
+skewness(my_data$frequency, na.rm = TRUE)
+skewness(my_data$polysemy, na.rm = TRUE)
+
+#plot normality after transformation
 ggdensity(my_data, x = "frequency", fill = "lightgray", title = "FREQ") +
   stat_overlay_normal_density(color = "red", linetype = "dashed")
 ggdensity(my_data, x = "polysemy", fill = "lightgray", title = "POLY") +
   stat_overlay_normal_density(color = "red", linetype = "dashed")
-skewness(my_data$frequency, na.rm = TRUE)
-skewness(my_data$polysemy, na.rm = TRUE)
-
-#plot the normality density after log transformation
-ggdensity(my_data$frequency, fill = "lightgray")
-ggdensity(my_data$polysemy, fill = "lightgray")
 
 #fit a binary logistic regression model
 model1 <- glm(change ~ my_data$frequency + my_data$polysemy + technology, data = my_data, family = binomial())
