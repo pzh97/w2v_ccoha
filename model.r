febe4f6533c5425b4000fbf3b979c9f98a5cfc61
pzh97 <55832928+pzh97@users.noreply.github.com>
2021-03-28 23:35:32 +0100

    Update model.r

--- model.r
+++ model.r
@@ -16,11 +16,9 @@ ggdensity(my_data$polysemy, fill = "lightgray")
 
 #log transformation
 my_data$frequency <- log10(my_data$frequency)
-my_data$polysemy <- log10(my_data$polysemy)
 
 #check normality after transformation
 skewness(my_data$frequency, na.rm = TRUE)
-skewness(my_data$polysemy, na.rm = TRUE)
 
 #plot normality after transformation
 ggdensity(my_data, x = "frequency", fill = "lightgray", title = "FREQ") +
@@ -29,29 +27,3 @@ ggdensity(my_data, x = "frequency", fill = "lightgray", title = "FREQ") +
 #fit a binary logistic regression model
 model1 <- glm(change ~ my_data$frequency + my_data$polysemy + technology, data = my_data, family = binomial())
 summary(model1)
-
-###################################################################################################################
-#ctree: run separately!
-library("party")
-library("plyr")
-library("readr")
-
-mydata <- read_csv("untitled copy.csv", col_names = FALSE)
-mydata<-rename(mydata, c("X1"="mydata.change", "X2"="mydata.frequency", "X3"="mydata.polysemy", "X4"="mydata.technology"))
-mydata$mydata.change<-as.factor(mydata$mydata.change) 
-mydata$mydata.frequency<-as.factor(mydata$mydata.frequency) 
-mydata$mydata.polysemy<-as.factor(mydata$mydata.polysemy) 
-mydata$mydata.technology<-as.factor(mydata$mydata.technology) 
-summary(mydata)
-tree<-ctree(mydata.change~mydata.frequency + mydata.polysemy + mydata.technology, data=mydata)
-plot(tree)
-####################################################################################################################
-#correlation test
-library(Hmisc)
-
-mydata<-read.csv(file.choose())
-mydata<-mydata[, c(2,3,4)]
-head(mydata, 6)
-res<-rcorr(as.matrix(mydata))
-####################################################################################################################
-res
